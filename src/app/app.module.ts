9e4decd445c5a81ae9e6615d9e6f31815ac1f132
Lucas Kostecki <lucas.kostecki@gmail.com>
2019-03-04 15:56:00 -0600

    Added checkbox form module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
@@ -10,7 +11,8 @@ import { PizzaComponent } from './pizza/pizza.component';
     PizzaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
