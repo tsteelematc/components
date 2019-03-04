91547f1cd78388f08847239dff44654570e32e5f
kellylyke <42480491+kellylyke@users.noreply.github.com>
2019-03-04 15:49:33 -0600

    update pizzaComponent.html
    added currency too

--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
+interface PizzaToppingDisplay {
+    name: string;
+    price: number;
+    checked: boolean;
+}
+
 @Component({
   selector: 'pizza-toppings',
   templateUrl: './pizza.component.html',
@@ -8,14 +14,17 @@ import { PizzaService } from '../pizza.service';
 })
 export class PizzaComponent implements OnInit {
 
-  //dependency inject the pizza service
-  constructor(public pizzaSvc: PizzaService) { }
+    availablePizzaToppings: PizzaToppingDisplay[];
+    total: number;
 
-  ngOnInit() {
-      //use pizza service to load available pizza toppings
-      const availablePizzaToppings = this.pizzaSvc.loadPizzaToppings();
-      console.log(availablePizzaToppings);
+    //dependency inject the pizza service
+    constructor(public pizzaSvc: PizzaService) { }
 
-  }
+    ngOnInit() {
+        //use pizza service to load available pizza toppings
+        this.availablePizzaToppings = this.pizzaSvc.loadPizzaToppings();
+       
+
+    }
 
 }
