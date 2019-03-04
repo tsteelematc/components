1a0e44b28b9d86350addd3753a54b2a48524fe58
Vivion Bahr <42786853+VivionB@users.noreply.github.com>
2019-03-04 15:40:45 -0600

    displaying pizza topping on web
    showing the pizza topping web

--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
-
+interface PizzaToppingDisplay{
+  name: string;
+  price: number;
+  checked: boolean;
+};
 @Component({
   selector: 'pizza-toppings',
   templateUrl: './pizza.component.html',
@@ -8,14 +12,16 @@ import { PizzaService } from '../pizza.service';
 })
 export class PizzaComponent implements OnInit {
 
+  avaiablePizzaToppings: PizzaToppingDisplay[];
+
   // DI (denendecy inject) the pizza service.
   constructor(public pizzaSvc: PizzaService) { }
 
   ngOnInit() {
     // Use pizza servise to load the avaiable pizza toppings.
     // var svc = new PizzaService() :-(
-      const avaiablePizzaToppings = this.pizzaSvc.loadPizzaTopping();
-      console.log(avaiablePizzaToppings);
+      this.avaiablePizzaToppings = this.pizzaSvc.loadPizzaTopping();
+      // console.log(avaiablePizzaToppings);
   }
 
 }
