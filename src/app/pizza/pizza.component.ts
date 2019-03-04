241a4cc852d91b4fc7350e5f8072dc1682109354
Luke <luke.busch@gmail.com>
2019-03-04 15:32:58 -0600

    Create pizza topping interface

--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
+interface PizzaToppingDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Component({
   selector: 'pizza-toppings',
   templateUrl: './pizza.component.html',
   styleUrls: ['./pizza.component.css']
 })
 export class PizzaComponent implements OnInit {
+  
+  availablePizzaToppings: PizzaToppingDisplay[];
+  total: number;
+  
   // Dependency inject the pizza service by specifying the service variable in the constructor of the component
   constructor(public pizzaSvc: PizzaService) { }
+
+  
 
   ngOnInit() {
     // use the pizza service to load available pizza toppings
     // gain access to pizza service via magical dependency injection
-    const availablePizzaToppings = this.pizzaSvc.loadPizzaToppings();
-    console.log(availablePizzaToppings);
+    this.availablePizzaToppings = this.pizzaSvc.loadPizzaToppings();
+    
   }
 
 }
