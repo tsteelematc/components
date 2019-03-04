e2a155bd55d3f9aa598c1276a6dd017bfa35761f
Belloche <dergiggler@gmail.com>
2019-03-04 15:28:08 -0600

    Implemented loadPizzaToppings and using it...
    Next, use it for real

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,16 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  public loadPizzaToppings() {
+    const pizzaToppingsFromWebService = [
+      { name: 'Pepperoni', price: 1.50 }
+      , { name: 'Olives', price: 3.50 }
+    ];
+
+    return pizzaToppingsFromWebService.map(x =>({
+      ...x
+      , checked: false
+    }));
+  }
 }
