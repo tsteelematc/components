174660f05121c891a80351638598b885472de964
Kathrin Rohrmeier (Köhler) <theliteratecondition@gmail.com>
2019-03-04 15:28:15 -0600

    implement loadPizzaToppings +
    ...and using it
    next: actually apply it

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
+// sets up for dependency-injection
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaService {
 
   constructor() { }
+
+  public loadPizzaToppings() {
+    const pizzaToppingsFromWebService = [
+      {name: 'Pepperoni', price: '1.50'}
+      , {name: 'Olive', price: '3.50'}
+      , {name: 'Mushroom', price: '2.50'}
+    ];
+
+    return pizzaToppingsFromWebService.map(x =>({
+      ...x
+      , checked: false
+    }));
+  }
 }
