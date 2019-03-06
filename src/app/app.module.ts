2e830ee54ac4252a46b720161cc8cf223652ee6c
Vivion Bahr <42786853+VivionB@users.noreply.github.com>
2019-03-06 16:03:03 -0600

    A Math component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
+import { MathComponent } from './math/math.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaComponent
+    PizzaComponent,
+    MathComponent
   ],
   imports: [
     BrowserModule
