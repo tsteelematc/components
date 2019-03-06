f184161114b36ee9b8401557a03d32c6d1294603
Aseng Vue <35584537+asevue@users.noreply.github.com>
2019-03-06 15:40:51 -0600

    Adding warning message if toppings wern't selected.

--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -43,6 +43,13 @@ export class PizzaComponent implements OnInit {
 
   }
 
+  // TypeScript getter property (or readonly)
+  get showWarning() {
+    // return this.availablePizzaToppings.filter(x => x.checked).length === 0;
+    return !this.availablePizzaToppings.some(x => x.checked);
+
+  }
+
   // DI (dependency inject) the pizza service.
   constructor(public pizzaSvc: PizzaService) { }
 
