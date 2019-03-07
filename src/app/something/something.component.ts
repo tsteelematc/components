e6dacb5174320327247e7fe8477390d65a91d471
Kat Kohler <39012778+KRohrmeier@users.noreply.github.com>
2019-03-07 14:31:33 -0600

    added numbers addition, added color addition...
    ...colors figured with ridiculous if/else if clause, but it works ;P
    
    Next time figure out how to limit & require two color choices.

--- src/app/something/something.component.ts
+++ src/app/something/something.component.ts
@@ -6,6 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./something.component.css']
 })
 export class SomethingComponent implements OnInit {
+  colors = [
+    {name:"red", checked: true}
+    , {name:"yellow", checked: true}
+    , {name:"blue", checked: false}
+  ];
+  foundColor = "";
+
+  firstNumber = 100;
+  secondNumber = 20;
+  result = 120;
+
+  addNumbers = () => {
+    this.result = this.firstNumber + this.secondNumber;
+  }
+
+  findColor = () => {
+    this.foundColor = "";
+    this.findChecked().forEach(element => {
+      this.foundColor += element.name;
+    });
+
+    if (this.foundColor == "yellow") {
+      this.foundColor = "yellow";
+    } else if (this.foundColor == "red") {
+      this.foundColor = "red";
+    } else if (this.foundColor == "blue") {
+      this.foundColor = "blue";
+    } else if (this.foundColor.includes("y") && this.foundColor.includes("r") && !this.foundColor.includes("b")) {
+      this.foundColor = "orange";
+    } else if (this.foundColor.includes("y") && this.foundColor.includes("b") && !this.foundColor.includes("r")) {
+      this.foundColor = "green";
+    } else if (this.foundColor.includes("r") && this.foundColor.includes("b") && !this.foundColor.includes("y")) {
+      this.foundColor = "purple";
+    } else {
+      this.foundColor = "brown";
+    }
+  }
+
+  findChecked = () => {
+    // console.log(this.colors.filter( function (x) { return x.checked == true;}));
+    let checkedColors = this.colors.filter( function (x) { return x.checked == true;});
+    return checkedColors;
+  }
+
 
   constructor() { }
 
