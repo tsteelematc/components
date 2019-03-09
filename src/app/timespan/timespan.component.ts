6012f37240213debbddf8d838b3d7ef853585be9
tjdombrowski <tjdombrowski@gmail.com>
2019-03-09 12:42:17 -0600

    Fixed a typo preventing the ending date from binding to the component

--- src/app/timespan/timespan.component.ts
+++ src/app/timespan/timespan.component.ts
@@ -12,7 +12,16 @@ export class TimespanComponent implements OnInit {
   age;
 
   calculateAge = () => {
-    
+    let dateOne = new Date(this.startingDate);
+    console.log(dateOne);
+    let dateTwo = new Date(this.endingDate);
+    console.log(dateTwo);
+    this.age = dateTwo.getFullYear() - dateOne.getFullYear();
+    console.log(this.age);
+    let months = dateTwo.getMonth() - dateOne.getMonth();
+    if (months < 0 || (months === 0 && dateTwo.getDate() < dateOne.getDate())) {
+        this.age--;
+    }
   }
 
   constructor() { }
