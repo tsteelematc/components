1a67f809aa8ed818b91eb55dc8c58fb1836e19b6
tjdombrowski <tjdombrowski@gmail.com>
2019-03-09 12:10:37 -0600

    Adding timespan component

---
+++ src/app/timespan/timespan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timespan',
+  templateUrl: './timespan.component.html',
+  styleUrls: ['./timespan.component.css']
+})
+export class TimespanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
