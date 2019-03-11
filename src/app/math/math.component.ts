c697896c155699965173ed1e7cddd822c9c89a87
Vivion Bahr <42786853+VivionB@users.noreply.github.com>
2019-03-11 15:35:22 -0500

    Code is complete
    Used Math.pow() and Math.sqrt()

--- src/app/math/math.component.ts
+++ src/app/math/math.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// interface NumbersToDisplay {
+//   numberOne: number;
+//   numberTwo: number;
+// }
+
 @Component({
   selector: 'app-math',
   templateUrl: './math.component.html',
@@ -7,15 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MathComponent implements OnInit {
 
+  // twoSetOfNumbers: NumbersToDisplay[];
   numberOne = 2;
   numberTwo = 3;
-  addTotal = 0;
-  totalNumbers;
+  totalNumbers = 0;
+  squNumber = 0;
+  squTotal = 0
+  powOne = 0;
+  powTwo = 0;
+  powTotal = 0;
 
   constructor() { }
 
    addEqual = () => {
-    this.addTotal = this.numberOne + this.numberTwo;
+    this.totalNumbers = this.numberOne + this.numberTwo;
+  }
+
+  squareEqual = () => {
+    this.squTotal = Math.sqrt(this.squNumber);
+  }
+
+  powerEqual = () => {
+    this.powTotal = Math.pow(this.powOne, this.powTwo);
   }
 
   ngOnInit() {
