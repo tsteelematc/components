90c0fbb811d157dbd995c386c1d413ea5bf99e80
Luke <luke.busch@gmail.com>
2019-03-11 15:02:47 -0500

    Generate the awesomest component
    Component now exists. Does nothing else (yet)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
+import { AwesomeComponentComponent } from './awesome-component/awesome-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaComponent
+    PizzaComponent,
+    AwesomeComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/awesome-component/awesome-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-awesome-component',
+  templateUrl: './awesome-component.component.html',
+  styleUrls: ['./awesome-component.component.css']
+})
+export class AwesomeComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
